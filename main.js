1fae7332541fc387b80e33efd87dffb6d7c75626
Akash Jaiswar <akashjaiswar221@gmai.com>
2024-09-07 14:16:17 +0530

    upload

---
+++ main.js
@@ -0,0 +1,9 @@
+<script>
+  const dfMessenger = document.querySelector('df-messenger'); // Set language to
+  English when the button is clicked
+  document.getElementById('englishBtn').addEventListener('click', function(){" "}
+  {dfMessenger.setAttribute("language-code", "en")}); // Set language to Hindi
+  when the button is clicked
+  document.getElementById('hindiBtn').addEventListener('click', function(){" "}
+  {dfMessenger.setAttribute("language-code", "hi")});
+</script>;
